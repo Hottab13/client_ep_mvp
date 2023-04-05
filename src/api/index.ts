4fed766a20b5aa5fb254cf66e0dc5d5d89454f1c
Hottab13 <alekseev.a@hotmail.com>
2023-04-05 21:00:02 +0300

    client_ep_01_14

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 //export const API_URL = "http://localhost:4741/api";
-export const API_URL = "https://server-ep-01.onrender.com/api";
+export const API_URL = "http://80.90.186.196";
 
 const instance = Axios.create({
   withCredentials: true,
