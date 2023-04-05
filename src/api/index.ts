8436d6103aa8dc170951e7d458995dd5984ea3aa
Hottab13 <alekseev.a@hotmail.com>
2023-04-05 21:16:24 +0300

    client_ep_01_16

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 //export const API_URL = "http://localhost:4741/api";
-export const API_URL = "http://80.90.186.196/api";
+export const API_URL = "http://80.90.186.196:4741/api";
 
 const instance = Axios.create({
   withCredentials: true,
